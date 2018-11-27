11891aa833389b1ac04c7afc465a2057da2cc773
Neel Shah <34845513+neelshah400@users.noreply.github.com>
2018-11-27 10:25:49 -0500

    Finish combine method
    Use a for loop to add each element of ArrayList b to ArrayList a, and return ArrayList a.

--- ArrayListDemo.java
+++ ArrayListDemo.java
@@ -9,19 +9,21 @@ public class ArrayListDemo{
 
 	}
 
-	public static ArrayList combine(ArrayList a, ArrayList b){
+	public static ArrayList combine(ArrayList<Integer> a, ArrayList<Integer> b){
 
+		for(int i = 0; i < b.size(); i++)
+			a.add(b.get(i));
 		return a;
 
 	}
 
-	public static void print(ArrayList list){
+	public static void print(ArrayList<Integer> list){
 
 		//
 
 	}
 
-	public static ArrayList removeDuplicates(ArrayList list){
+	public static ArrayList removeDuplicates(ArrayList<Integer> list){
 
 		return list;
 
