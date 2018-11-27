c25a4334fad22f0aa2dea3d5347787291b5f2457
Neel Shah <34845513+neelshah400@users.noreply.github.com>
2018-11-27 10:22:21 -0500

    Create skeleton
    Added main, combine, print, and removeDuplicates methods.

---
+++ ArrayListDemo.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+
+public class ArrayListDemo{
+
+	public static void main(String[]args){
+
+		//
+
+	}
+
+	public static ArrayList combine(ArrayList a, ArrayList b){
+
+		return a;
+
+	}
+
+	public static void print(ArrayList list){
+
+		//
+
+	}
+
+	public static ArrayList removeDuplicates(ArrayList list){
+
+		return list;
+
+	}
+
+}
